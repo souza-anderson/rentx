c5801742e40eab4c4a1ce58687670a0ea764738e
Anderson <andersonfebs@gmail.com>
2022-04-21 23:51:35 -0400

    Refatora a aplicação 2

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,9 @@
 import { container } from "tsyringe";
 
-import { UsersRepository } from "@modules/accounts/infra/repositories/UsersRepository";
+import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
-import { CategoriesRepository } from "@modules/cars/infra/repositories/CategoriesRepository";
-import { SpecificationsRepository } from "@modules/cars/infra/repositories/SpecificationsRepository";
+import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
+import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationsRepository";
 import { ICategoriesRespository } from "@modules/cars/repositories/ICategoriesRepository";
 import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 
