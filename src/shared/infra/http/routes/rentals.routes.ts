2030d7a5e91d2661f82537899fa35b5cfb107230
Anderson <andersonfebs@gmail.com>
2022-06-04 00:01:13 -0400

    Criando controller de rental

---
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
+
+import { ensureAuthenticated } from "../middlewares/ensureAthenticated";
+
+const rentalsRoutes = Router();
+
+const createRentalController = new CreateRentalController();
+
+rentalsRoutes.post("/", ensureAuthenticated, createRentalController.handle);
+
+export { rentalsRoutes };
