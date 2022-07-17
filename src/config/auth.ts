42f076d225ecf3d7a76813dfe9447e59fb2b4541
Anderson <andersonfebs@gmail.com>
2022-07-17 11:09:05 -0400

    Refatorando autenticação do usuário

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "6788e0c18c6755ec06af01ca88ef9658",
+  expires_in_token: "15m",
+  secret_refresh_token: "eab381e714433747a4941f1642e89e03",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
