db46666dedd3e3a9cde15b1e5811f566c2980823
Anderson <andersonfebs@gmail.com>
2022-07-26 07:53:57 -0400

    Inserindo template engine para envio de e-mail

--- src/shared/container/providers/MailProvider/IMailProvider.ts
+++ src/shared/container/providers/MailProvider/IMailProvider.ts
@@ -1,5 +1,10 @@
 interface IMailProvider {
-  sendMail(to: string, subject: string, body: string): Promise<void>;
+  sendMail(
+    to: string,
+    subject: string,
+    varibles: any,
+    path: string
+  ): Promise<void>;
 }
 
 export { IMailProvider };
