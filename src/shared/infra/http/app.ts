3261ff16930113d124c1e2c8a587d760755565b4
Anderson <andersonfebs@gmail.com>
2022-08-22 09:13:32 -0400

    update upload image car

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -8,6 +8,8 @@ import { AppError } from "@shared/errors/AppError";
 import createConnection from "@shared/infra/typeorm";
 import "@shared/container";
 
+import upload from "@config/upload";
+
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
 
@@ -15,6 +17,8 @@ createConnection();
 const app = express();
 
 app.use(express.json());
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
